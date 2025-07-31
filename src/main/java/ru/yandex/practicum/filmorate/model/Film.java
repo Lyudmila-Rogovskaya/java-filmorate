c9b0229ab563dae58beab08e4d74d9acbe78f953
Lyudmila Rogovskaya <lyussenok@yandex.ru>
2025-08-01 01:41:54 +0700

    Исправить замечания после ревью - спринт 10 (не компилируется - версия 2)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+//import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 /**
  * Film.
  */
-@Data
+//@Data
 @Getter
 @Setter
 public class Film { // модель, описывающая фильмы
