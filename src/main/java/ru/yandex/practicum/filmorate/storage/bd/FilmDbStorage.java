800324bdddde16a792857aaed1d51c9786690891
Lyudmila Rogovskaya <lyussenok@yandex.ru>
2025-09-12 06:27:52 +0700

    Выполнить техническое задание - спринт 12 (после 1 проверки - исправлены замечания, постман - ок, юнит - лежат)

--- src/main/java/ru/yandex/practicum/filmorate/storage/bd/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/bd/FilmDbStorage.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.GenreStorage;
 import ru.yandex.practicum.filmorate.storage.MpaStorage;
 
 import java.sql.Date;
